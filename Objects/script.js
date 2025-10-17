d2492633ce51ee3c17c0ff9dfdeb9c3d9b864269
Azaan Khan <azaan.ak001@example.com>
2025-10-17 21:28:40 +0530

    Object(Dictionary)

---
+++ Objects/script.js
@@ -0,0 +1,12 @@
+//Creating Objects (Dictionary)
+const Person = {
+    name : 'Alice',age:25,city:"New York"
+};
+//Dynamic Mnaipulation
+//1. Dot Notation
+Person.salary = 25000; //This is dot notation 
+
+//2. Bracket Notation
+Person['isMarried'] = 'False';
+console.log(Person);
+
